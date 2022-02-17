2ea10fc53c9905f0a617e934e8eaad8f6905b51f
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-02-17 12:58:10 +0000

    adding controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+num_movies = 10
+
+puts "SEEDING BEGIN"
+puts "  creating #{num_movies} fake movies..."
+
+# Movie.destroy_all # uncomment if you want to clear the existing db
+
+num_movies.times do
+  movie = Movie.new(
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
+    poster_url: Faker::Internet.url,
+    rating: [1..10].select
+  )
+  if movie.save
+    puts "    added movie #{movie.title}, #{movie.overview} to the DB!"
+  else
+    puts "    ERROR: failed to save movie #{movie.title}, #{movie.overview} to the DB :("
+    puts "      REASON: #{movie.errors.inspect}"
+  end
+end
+puts "  seeding finished, the movies database has #{Movie.count} entries"
+puts "SEEDING END"

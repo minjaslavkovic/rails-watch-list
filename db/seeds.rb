73177bcda7f4171aed3205db6defae7a6cb78929
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-03-18 12:57:09 +0000

    moved navbar into a shared folder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,26 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-num_movies = 10
+# num_movies = 10
 
-puts "SEEDING BEGIN"
-puts "  creating #{num_movies} fake movies..."
+# puts "SEEDING BEGIN"
+# puts "  creating #{num_movies} fake movies..."
 
 # Movie.destroy_all # uncomment if you want to clear the existing db
 
-num_movies.times do
-  movie = Movie.new(
-    title: Faker::Movie.title,
-    overview: Faker::Movie.quote,
-    poster_url: Faker::Internet.url,
-    rating: [1..10].select
-  )
-  if movie.save
-    puts "    added movie #{movie.title}, #{movie.overview} to the DB!"
-  else
-    puts "    ERROR: failed to save movie #{movie.title}, #{movie.overview} to the DB :("
-    puts "    REASON: #{movie.errors.inspect}"
-  end
-end
-puts "  seeding finished, the movies database has #{Movie.count} entries"
-puts "SEEDING END"
+# num_movies.times do
+#   movie = Movie.new(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#     poster_url: Faker::Internet.url,
+#     rating: [1..10].select
+#   )
+#   if movie.save
+#     puts "    added movie #{movie.title}, #{movie.overview} to the DB!"
+#   else
+#     puts "    ERROR: failed to save movie #{movie.title}, #{movie.overview} to the DB :("
+#     puts "    REASON: #{movie.errors.inspect}"
+#   end
+# end
+# puts "  seeding finished, the movies database has #{Movie.count} entries"
+# puts "SEEDING END"
+
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)

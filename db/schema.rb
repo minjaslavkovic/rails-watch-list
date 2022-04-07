2dbb0b6337711b52533605857b068db4c9d33237
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-04-08 00:00:57 +0100

    Changed rating integer to flaot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_152644) do
+ActiveRecord::Schema.define(version: 2022_04_07_224408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_152644) do
     t.string "title"
     t.text "overview"
     t.string "poster_url"
-    t.integer "rating"
+    t.float "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
